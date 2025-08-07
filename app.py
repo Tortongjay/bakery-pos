ac339fb536d399b6efd23cce52d67b09be23dcae
drexxm <65310167@go.buu.ac.th>
2025-08-07 16:21:43 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+import datetime
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        item = request.form['item']
+        quantity = int(request.form['quantity'])
+        price = float(request.form['price'])
+
+        total = quantity * price
+        time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        df = pd.DataFrame([[time, item, quantity, price, total]],
+                          columns=['Time', 'Item', 'Quantity', 'Price', 'Total'])
+        df.to_csv('sales.csv', mode='a', header=not pd.io.common.file_exists('sales.csv'), index=False)
+
+        return redirect('/')
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
